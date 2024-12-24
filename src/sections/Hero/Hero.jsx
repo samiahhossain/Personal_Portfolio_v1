00d26a082ca84c3bece8991e8ed942cb5d327e69
samiahhossain <samiah.hoss@gmail.com>
2024-12-24 17:43:27 -0400

    Main front view

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,60 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import { useTheme } from '../../common/ThemeContext';
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={heroImg}
+          alt="profile picture of Samiah Hossain"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Samiah
+          <br />
+          Hossain
+        </h1>
+        <h2>Developer.</h2>
+        <span>
+          <a href="https://github.com/samiahhossain" target="_blank">
+            <img src={githubIcon} alt="github icon" />
+          </a>
+          <a href="https://linkedin.com/in/samiahh" target="_blank">
+            <img src={linkedinIcon} alt="linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          Developing and analyzing all things tech.
+        </p>
+        {/* EITHER POPUP OR EMAIL TEMPLATE */}
+        <a href="">
+          <button className="hover">Request Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,16 @@
+import styles from './ProjectsStyles.module.css';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <a href="https://github.com/samiahhossain/TLDR_AI">
+          <img className="hover" src="" alt="" />
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
