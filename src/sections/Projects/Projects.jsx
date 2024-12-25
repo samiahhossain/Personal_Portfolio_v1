af4710e6d14198b4a59f0cb7e31e8db2e588f89f
samiahhossain <samiah.hoss@gmail.com>
2024-12-24 20:01:03 -0400

    Added projects with image placeholders

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,13 +1,39 @@
 import styles from './ProjectsStyles.module.css';
+import TLDR from '../../assets/viberr.png';
+import scribbleSpace from '../../assets/fresh-burger.png'
+import ziShi from '../../assets/hipsster.png'
+import invMonitor from '../../assets/fitlift.png'
+import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a href="https://github.com/samiahhossain/TLDR_AI">
-          <img className="hover" src="" alt="" />
-        </a>
+      <ProjectCard
+          src={invMonitor}
+          link="https://github.com/samiahhossain/InvMonitor"
+          h3="Inventory Monitor"
+          p="Simulating Usage"
+        />
+        <ProjectCard
+          src={TLDR}
+          link="https://github.com/samiahhossain/TLDR_AI"
+          h3="TL;DR AI"
+          p="Intelligent Summarizer"
+        />
+        <ProjectCard
+          src={scribbleSpace}
+          link="https://github.com/samiahhossain/ScribbleSpace"
+          h3="ScribbleSpace"
+          p="Notes Application"
+        />
+        <ProjectCard
+          src={ziShi}
+          link="https://github.com/samiahhossain/ZiShi-s_Wardrobe"
+          h3="ZiShi's Wardrobe"
+          p="Clothing Catalogue"
+        />
       </div>
     </section>
   );
