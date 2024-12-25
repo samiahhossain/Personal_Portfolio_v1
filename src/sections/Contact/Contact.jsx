dd4105ba78e8fbc308f8ae2be53f6737c46c6f51
samiahhossain <samiah.hoss@gmail.com>
2024-12-24 21:04:57 -0400

    Most content added

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+import styles from './ContactStyles.module.css';
+
+function Contact() {
+  return (
+    <section id="contact" className={styles.container}>
+      <h1 className="sectionTitle">Contact</h1>
+      
+            <p>Connect and/or chat with me on LinkedIn!</p>
+        
+        
+            <p>Email me at samiah.hossain@dal.ca.</p>
+      
+    </section>
+  );
+}
+
+export default Contact;
