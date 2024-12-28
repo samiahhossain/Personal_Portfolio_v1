c70b904e8015650ccdf7e1e0f938fa6ae31618d0
samiahhossain <samiah.hoss@gmail.com>
2024-12-27 22:15:13 -0400

    About wording and contact icon tweaks

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <section id="about" className={styles.container}>
       <h1 className="sectionTitle">About</h1>
       <p className={styles.description}>
-        I am an undergraduate computer science student at Dalhousie University, currently in my second year. My curiousity in tech in high school, when I taught myself basic web development. I was drawn to the control, flexibility, and customizability I had with the ability to code and use technical tools effectively.
+        I am an undergraduate computer science student at Dalhousie University, currently in my second year. My curiousity in tech began in high school, when I taught myself basic web development. I was drawn to the control, flexibility, and customizability I had with the ability to code and use technical tools effectively.
         Fast forward to today, I am most excited about full stack software development and cybersecurity, and I also possess skills and knowledge in cloud computing, artificial intelligence, data analytics, DevOps, and web design. 
         <br /><br />
         I am always working to gain new skills and expand on the ones I already have. Read on to see what I have learnt and accomplished so far.
