0cebb1f814ffeeeb18eefa1c03beb3dae9cae50d
samiahhossain <samiah.hoss@gmail.com>
2025-01-08 22:08:47 -0400

    Added vague work details to About

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -6,7 +6,7 @@ function About() {
       <h1 className="sectionTitle">About</h1>
       <p className={styles.description}>
         I am an undergraduate computer science student at Dalhousie University, currently in my second year. My curiousity in tech began in high school, when I taught myself basic web development. I was drawn to the control, flexibility, and customizability I had with the ability to code and use technical tools effectively.
-        Fast forward to today, I am most excited about full stack software development and cybersecurity, and I also possess skills and knowledge in cloud computing, artificial intelligence, data analytics, DevOps, and web design. 
+        Fast forward to today, I am most excited about full stack software development and cybersecurity, and I also possess skills and knowledge in cloud computing, artificial intelligence, data analytics, DevOps, and web design. I've had the privilege of creating a website to further ocean science education, and sharing my love for tech and its different applications with youth across Nova Scotia.
         <br /><br />
         I am always working to gain new skills and expand on the ones I already have. Read on to see what I have learnt and accomplished so far.
       </p>
